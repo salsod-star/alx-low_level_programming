de95075f50cb23f2559517f6787ba539e240550c
Salaudeen Sodiq <salsod1st@gmail.com>
2022-03-12 04:07:10 -0800

    final commit on this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
+/* Programming to print out a string of text using puts */
+
 #include <stdio.h>
-int main(){
+int main(void) /* code execution begins here */
+{
 	puts("Programming is like building a multilingual puzzle \n");
-	return 0;
+	return (0); /* end of program execution */
 }

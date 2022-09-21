be3399ae996334409f491af9c71aa1accc817f48
sodiq <salsod1st@gmail.com>
2022-09-21 07:28:11 -0700

    First commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/*
+ * main description: Program to print alphabet letter followed by new line
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+# Variables, if, else, while in C Language
+Solving all given task under C variable, conditionals and loops syntax

3309474e6d7b80be5b3897b3ffd14dca14a82133
sodiq <salsod1st@gmail.com>
2022-09-22 05:29:35 -0700

    print all alphabets in lower case 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * main - print all alphabets in lowercase 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
+	}
+}
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}

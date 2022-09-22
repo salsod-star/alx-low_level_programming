5dcc6a21250b37662414460deece06a93e142b9c
sodiq <salsod1st@gmail.com>
2022-09-22 05:07:34 -0700

    print out all alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * main - print out all alhaphets in lowercase
+ *
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+	int a = 98;
+
+	for (; a < 122; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar("\n");
+}
